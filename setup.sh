4d7ef90ec57b7cd5cb7fa205647fc7fe06f09e42
xtakei <xtakei@me.com>
2016-08-24 11:46:37 +0900

    setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+
+
+# mv .zprofile ~/git/dotfiles/.
+# mv .zshrc ~/git/dotfiles/.
+# mv .vim ~/git/dotfiles/.
+# mv .vimrc ~/git/dotfiles/. 
+
+cd $HOME
+ln -sf ~/git/dotfiles/.zprofile .zprofile
+ln -sf ~/git/dotfiles/.zshrc .zshrc
+ln -sf ~/git/dotfiles/.vim .vim
+ln -sf ~/git/dotfiles/.vimrc .vimrc
